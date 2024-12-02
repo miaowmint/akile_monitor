aabe04a9bd4d999fed9b62d969b2f6eb821768d7
猫猫摸大鱼 <58991169+miaowmint@users.noreply.github.com>
2024-12-03 06:33:41 +0800

    Update ak_client.sh

--- ak_client.sh
+++ ak_client.sh
@@ -62,7 +62,7 @@ install_akile_monitor_client(){
     # 启用服务，使其开机自启动
     systemctl daemon-reload
     systemctl enable ak_client
-    systemctl restart ak_client
+    systemctl start ak_client
     
     cat /etc/ak_monitor/client.json
 }
